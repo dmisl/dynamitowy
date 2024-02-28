6c829dd040014ba10358123a81eed19a7b571697
dmisl <rithlesscorp@gmail.com>
2024-02-28 23:43:47 +0100

    journal beta

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Lesson;
+use App\Models\Subject;
 use App\Models\Teacher;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,9 +15,9 @@ class JournalController extends Controller
     {
         $today = date('d.m.Y');
         $teacher = Auth::user()->teacher;
-        $todayLessons = [];
-        
-        dd(Auth::user()->teacher->id);
-        return view('journal.index', compact('today'));
+        $timetable = ['0' => '7:10', '1' => '8:00', '2' => '8:50', '3' => '9:40', '4' => '10:30', '5' => '11:35', '6' => '12:25', '7' => '13:15', '8' => '14:05', '9' => '14:55', '10' => '15:45', '11' => '16:35',];
+        $subjects = Subject::query()->where(['teacher_id' => Auth::user()->teacher->id])->get('id');
+        $lessons = Lesson::query()->where(['day' => date('n')+1])->whereIn('subject_id', $subjects)->get();
+        return view('journal.index', compact('today', 'lessons', 'timetable', 'subjects'));
     }
 }
--- resources/views/journal/index.blade.php
+++ resources/views/journal/index.blade.php
@@ -10,14 +10,36 @@
                 <select class="form-select rounded-0">
                     <option value="">{{ $today }}</option>
                 </select>
-                
+                @foreach($lessons as $lesson)
+                    <div class="m-0 p-0 border-bottom border-dark">
+                        <p class="p-0 m-0 ps-1 pt-1 fw-medium">{{ $lesson->classroom->name }} - {{ $timetable[$lesson->lesson_number] }}</p>
+                        <p class="p-0 m-0 ps-1 small text-wrap">{{ App\Models\Subject::find($lesson->subject_id)->name }}</p>
+                    </div>
+                @endforeach
             </div>
         </div>
         <div class="col-md-9">
 
             <div class="p-3">
 
-
+                <table class="table table-primary">
+                    <tr>
+                        <th scope="col">Imie nazwisko ucznia</th>
+                        @foreach(App\Models\Lesson::query()->where(['classroom_id' => $lessons->first()->classroom_id, 'day' => $lessons->first()->day])->get() as $lesson)
+                            <th>{{ $timetable[$lesson->lesson_number] }}</th>
+                        @endforeach
+                    </tr>
+                    <tr class="">
+                        <td scope="row">R1C1</td>
+                        <td>R1C2</td>
+                        <td>R1C3</td>
+                    </tr>
+                    <tr class="">
+                        <td scope="row">Item</td>
+                        <td>Item</td>
+                        <td>Item</td>
+                    </tr>
+                </table>
 
             </div>
 
