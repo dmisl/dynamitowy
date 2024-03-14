97d56b2d58de7d22a55f01cf52dcb2d1c10beb29
unknown <rithlesscorp@gmail.com>
2024-03-14 13:55:04 +0100

    update method (not-completed) added to GradeController

--- resources/js/components/classroom/Parent.vue
+++ resources/js/components/classroom/Parent.vue
@@ -1,6 +1,33 @@
 <template>
-    <div>
-        <component :is="currentComponent" :classroom_id="props.classroom_id" :user_id="user_id"></component>
+    <div class="d-flex">
+        <div style="height: 100%;">
+            <div class="col-md-3 bg-info bg-gradient" style="position: fixed; height: 100vh;">
+                <div class="user-select-none">
+                    
+                    <a role="button" @click="change(rawIndex)" class="text-decoration-none text-dark">
+                        <div class="w-100 ps-2 fs-5 border-bottom border-dark">
+                            Zarzadzanie uczniami
+                        </div>
+                    </a>
+                    <a role="button" @click="change(rawTimetable)" class="text-decoration-none text-dark">
+                        <div class="w-100 ps-2 fs-5 border-bottom border-dark">
+                            Plan lekcji
+                        </div>
+                    </a>
+                    <a role="button" @click="change(rawTimetableEdit)" class="text-decoration-none text-dark">
+                        <div class="w-100 ps-2 fs-5 border-bottom border-dark">
+                            Edytowac plan lekcji
+                        </div>
+                    </a>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3"></div>
+
+        <div class="col-md-9">
+            <component :is="currentComponent" :classroom_id="props.classroom_id" :user_id="user_id"></component>
+        </div>
     </div>
 </template>
 
